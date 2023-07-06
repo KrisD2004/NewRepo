47ebe29f7a5fd203eb72879b9abd175a92072b5e
Kristian Dotson <KDotson387@gmail.com>
2023-07-06 09:52:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prepWorkcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
